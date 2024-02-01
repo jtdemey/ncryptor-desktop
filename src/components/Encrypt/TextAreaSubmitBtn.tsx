0febdb432e0938f7b32f6d3d34c564bb94feeff4
jtdemey <johntdemey@live.com>
2024-02-01 00:03:24 -0500

    Submit button refactor

---
+++ src/components/Encrypt/TextAreaSubmitBtn.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import { invokeTauriCommand } from "../../services/invokeTauriCommand";
+
+type TextAreaSubmitBtnProps = {
+  serviceName: string;
+  serviceParams: any;
+  label: string;
+  setText: Function;
+  text: string;
+};
+
+const Button = styled.div`
+  margin: 1rem 0 auto auto;
+  padding: 0.5rem 1rem;
+  background: #52796f;
+  border: 1px solid #52796f;
+  box-shadow: -3px 3px 8px #222;
+  color: #cad2c5;
+  font-family: "Lato", sans-serif;
+  font-size: 1.1rem;
+  font-weight: bold;
+  text-align: center;
+  text-shadow: 2px 2px 0px rgba(0, 0, 0, 0.2);
+`;
+
+const TextAreaSubmitBtn = ({
+  serviceName,
+  serviceParams,
+  label,
+  setText,
+  text,
+}: TextAreaSubmitBtnProps): JSX.Element => {
+  const [loading, setLoading] = React.useState(false);
+  const clickFunc = async () =>
+    invokeTauriCommand(serviceName, {
+      ...serviceParams,
+      text,
+    }).then((result: string) => {
+      console.log(result);
+      setLoading(false);
+      setText(result);
+    });
+  return (
+    <Button onClick={() => clickFunc()}>{loading ? "Loading" : label}</Button>
+  );
+};
+
+export default TextAreaSubmitBtn;
